9a41090aae0b347a69d463821087785562a7b629
XXXDENDACION <icesfrost@gmail.com>
2019-12-17 21:33:09 +0300

    add initial function(last elements)

--- learnjs.js
+++ learnjs.js
@@ -1,4 +1,4 @@
-function first(array, n = 1) {
+function first(array = [], n = 1) {
     if(array == null) return 0;
     let retarr = [];
     for(let i = 0; i < n ; i++)
@@ -7,4 +7,18 @@ function first(array, n = 1) {
     }
     return retarr;
   };
-  alert(first([1,2,3,4,5]));
+
+  function initial(array = [], n = 1)
+  {
+      if(array == null) return 0;
+      let retarr =[];
+      for(let i = 0; i != array.length - n; i++)
+      {
+          retarr[i] = array[i];
+      }
+      return retarr;
+  }
+
+  console.log(first([1,2,3,4,5]));
+  console.log(initial([1,2,3,4,5]));
+  
